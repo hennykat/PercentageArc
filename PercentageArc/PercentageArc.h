bb232ae521d74cd7133c5a8c00b70d92eccffbd9
Hennessy <hennykdev@gmail.com>
2018-02-04 12:40:34 -0600

    added proj

---
+++ PercentageArc/PercentageArc.h
@@ -0,0 +1,19 @@
+//
+//  PercentageArc.h
+//  PercentageArc
+//
+//  Created by Emily Hennessy on 2/4/18.
+//  Copyright © 2018 hennykat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PercentageArc.
+FOUNDATION_EXPORT double PercentageArcVersionNumber;
+
+//! Project version string for PercentageArc.
+FOUNDATION_EXPORT const unsigned char PercentageArcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PercentageArc/PublicHeader.h>
+
+
