63aff2d354017028d991f4e2cb79ccc55ccae1f7
Hennessy <hennykdev@gmail.com>
2018-02-06 18:24:40 -0600

    renamed var

--- PercentageArcTests/util/PercentageArcUtilTest.swift
+++ PercentageArcTests/util/PercentageArcUtilTest.swift
@@ -12,121 +12,121 @@ class PercentageArcUtilTest: XCTestCase {
     }
     
     func test1MakeInRange() {
-        let actual = PercentageArcUtil.makeInRange(value: 0.5)
-        XCTAssertEqual(actual, 0.5, "input: valid")
+        let result = PercentageArcUtil.makeInRange(value: 0.5)
+        XCTAssertEqual(result, 0.5, "input: valid")
     }
     
     func test2MakeInRange() {
-        let actual = PercentageArcUtil.makeInRange(value: 0)
-        XCTAssertEqual(actual, 0, "input: valid")
+        let result = PercentageArcUtil.makeInRange(value: 0)
+        XCTAssertEqual(result, 0, "input: valid")
     }
     
     func test3MakeInRange() {
-        let actual = PercentageArcUtil.makeInRange(value: 1)
-        XCTAssertEqual(actual, 1, "input: valid")
+        let result = PercentageArcUtil.makeInRange(value: 1)
+        XCTAssertEqual(result, 1, "input: valid")
     }
     
     func test4MakeInRange() {
-        let actual = PercentageArcUtil.makeInRange(value: -0.5)
-        XCTAssertEqual(actual, 0, "input: invalid")
+        let result = PercentageArcUtil.makeInRange(value: -0.5)
+        XCTAssertEqual(result, 0, "input: invalid")
     }
     
     func test5MakeInRange() {
-        let actual = PercentageArcUtil.makeInRange(value: 2)
-        XCTAssertEqual(actual, 0, "input: invalid")
+        let result = PercentageArcUtil.makeInRange(value: 2)
+        XCTAssertEqual(result, 0, "input: invalid")
     }
     
     func test1FindCentre() {
-        let actual = PercentageArcUtil.findCentre(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
-        XCTAssertEqual(actual.x, 50, "input: 100 x 100 sq")
-        XCTAssertEqual(actual.y, 50, "input: 100 x 100 sq")
+        let result = PercentageArcUtil.findCentre(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
+        XCTAssertEqual(result.x, 50, "input: 100 x 100 sq")
+        XCTAssertEqual(result.y, 50, "input: 100 x 100 sq")
     }
     
     func test2FindCentre() {
-        let actual = PercentageArcUtil.findCentre(rect: CGRect(x: 1, y: 8, width: 20, height: 30))
-        XCTAssertEqual(actual.x, 10, "input: 20 x 30 rect")
-        XCTAssertEqual(actual.y, 15, "input: 20 x 30 rect")
+        let result = PercentageArcUtil.findCentre(rect: CGRect(x: 1, y: 8, width: 20, height: 30))
+        XCTAssertEqual(result.x, 10, "input: 20 x 30 rect")
+        XCTAssertEqual(result.y, 15, "input: 20 x 30 rect")
     }
     
     func test3FindCentre() {
-        let actual = PercentageArcUtil.findCentre(rect: CGRect(x: 3, y: 1, width: 0.5, height: 3))
-        XCTAssertEqual(actual.x, 0.25, "input: 0.5 x 3 rect")
-        XCTAssertEqual(actual.y, 1.5, "input: 0.5 x 3 rect")
+        let result = PercentageArcUtil.findCentre(rect: CGRect(x: 3, y: 1, width: 0.5, height: 3))
+        XCTAssertEqual(result.x, 0.25, "input: 0.5 x 3 rect")
+        XCTAssertEqual(result.y, 1.5, "input: 0.5 x 3 rect")
     }
     
     func test1FindRadius() {
-        let actual = PercentageArcUtil.findRadius(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
-        XCTAssertEqual(actual, 50, "input: 100 x 100 sq")
+        let result = PercentageArcUtil.findRadius(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
+        XCTAssertEqual(result, 50, "input: 100 x 100 sq")
     }
     
     func test2FindRadius() {
-        let actual = PercentageArcUtil.findRadius(rect: CGRect(x: 3, y: 20, width: 30, height: 100))
-        XCTAssertEqual(actual, 15, "input: 30 x 100 rect")
+        let result = PercentageArcUtil.findRadius(rect: CGRect(x: 3, y: 20, width: 30, height: 100))
+        XCTAssertEqual(result, 15, "input: 30 x 100 rect")
     }
     
     func test3FindRadius() {
-        let actual = PercentageArcUtil.findRadius(rect: CGRect(x: 3, y: 1, width: 0.5, height: 3))
-        XCTAssertEqual(actual, 0.25, "input: 0.5 x 3 rect")
+        let result = PercentageArcUtil.findRadius(rect: CGRect(x: 3, y: 1, width: 0.5, height: 3))
+        XCTAssertEqual(result, 0.25, "input: 0.5 x 3 rect")
     }
     
     func test1GetStartAngle() {
-        let actual = PercentageArcUtil.getStartAngle(start: .top)
-        XCTAssertEqual(actual, CGFloat.pi * 3 / 2, "input: top")
+        let result = PercentageArcUtil.getStartAngle(start: .top)
+        XCTAssertEqual(result, CGFloat.pi * 3 / 2, "input: top")
     }
     
     func test2GetStartAngle() {
-        let actual = PercentageArcUtil.getStartAngle(start: .right)
-        XCTAssertEqual(actual, 0, "input: right")
+        let result = PercentageArcUtil.getStartAngle(start: .right)
+        XCTAssertEqual(result, 0, "input: right")
     }
     
     func test3GetStartAngle() {
-        let actual = PercentageArcUtil.getStartAngle(start: .bottom)
-        XCTAssertEqual(actual, CGFloat.pi / 2, "input: bottom")
+        let result = PercentageArcUtil.getStartAngle(start: .bottom)
+        XCTAssertEqual(result, CGFloat.pi / 2, "input: bottom")
     }
     
     func test4GetStartAngle() {
-        let actual = PercentageArcUtil.getStartAngle(start: .left)
-        XCTAssertEqual(actual, CGFloat.pi, "input: left")
+        let result = PercentageArcUtil.getStartAngle(start: .left)
+        XCTAssertEqual(result, CGFloat.pi, "input: left")
     }
     
     func test1GetFullAngle() {
-        let actual = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.top)
-        XCTAssertEqual(actual, CGFloat.pi * 7 / 2, "input: top")
+        let result = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.top)
+        XCTAssertEqual(result, CGFloat.pi * 7 / 2, "input: top")
     }
     
     func test2GetFullAngle() {
-        let actual = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.right)
-        XCTAssertEqual(actual, CGFloat.pi * 2, "input: right")
+        let result = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.right)
+        XCTAssertEqual(result, CGFloat.pi * 2, "input: right")
     }
     
     func test3GetFullAngle() {
-        let actual = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.bottom)
-        XCTAssertEqual(actual, CGFloat.pi * 5 / 2, "input: bottom")
+        let result = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.bottom)
+        XCTAssertEqual(result, CGFloat.pi * 5 / 2, "input: bottom")
     }
     
     func test4GetFullAngle() {
-        let actual = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.left)
-        XCTAssertEqual(actual, CGFloat.pi * 3, "input: left")
+        let result = PercentageArcUtil.getFullAngle(startAngle: PercentageArcUtil.left)
+        XCTAssertEqual(result, CGFloat.pi * 3, "input: left")
     }
     
     func test1MakePercentageAngle() {
-        let actual = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.top)
-        XCTAssertEqual(actual, CGFloat.pi * 5 / 2, "input: top to 50%")
+        let result = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.top)
+        XCTAssertEqual(result, CGFloat.pi * 5 / 2, "input: top to 50%")
     }
     
     func test2MakePercentageAngle() {
-        let actual = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.right)
-        XCTAssertEqual(actual, CGFloat.pi, "input: right to 50%")
+        let result = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.right)
+        XCTAssertEqual(result, CGFloat.pi, "input: right to 50%")
     }
     
     func test3MakePercentageAngle() {
-        let actual = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.bottom)
-        XCTAssertEqual(actual, CGFloat.pi * 3 / 2, "input: bottom to 50%")
+        let result = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.bottom)
+        XCTAssertEqual(result, CGFloat.pi * 3 / 2, "input: bottom to 50%")
     }
     
     func test4MakePercentageAngle() {
-        let actual = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.left)
-        XCTAssertEqual(actual, 2 * CGFloat.pi, "input: left to 50%")
+        let result = PercentageArcUtil.makePercentageAngle(percentage: 0.5, start: PercentageArcUtil.left)
+        XCTAssertEqual(result, 2 * CGFloat.pi, "input: left to 50%")
     }
     
 }
